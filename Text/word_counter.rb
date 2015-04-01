ddf5c041649a05d2cba9a8df480bfa74d3364f69
Jose Tobar <ilovemysillybanana@gmail.com>
2015-04-01 16:00:58 -0400

    Fixed

--- Text/word_counter.rb
+++ Text/word_counter.rb
@@ -2,7 +2,7 @@ puts "Please input a sentence to count"
 userInput = gets.chomp
 puts userInput
 
-@wordCount=0
+@wordCount=1
 (0..userInput.length).each do |char|
   if userInput[char] == ' ' #also tried .equal but didn't work
     @wordCount = @wordCount + 1

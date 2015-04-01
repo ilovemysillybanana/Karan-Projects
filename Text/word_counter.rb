dc08cfa2d8f7591cf2a44c867b7578263c855e46
Jose Tobar <ilovemysillybanana@gmail.com>
2015-04-01 16:02:31 -0400

    Added comment to word_count.rb

--- Text/word_counter.rb
+++ Text/word_counter.rb
@@ -2,6 +2,7 @@ puts "Please input a sentence to count"
 userInput = gets.chomp
 puts userInput
 
+#can't count the first word, so add it to the count anyway
 @wordCount=1
 (0..userInput.length).each do |char|
   if userInput[char] == ' ' #also tried .equal but didn't work
